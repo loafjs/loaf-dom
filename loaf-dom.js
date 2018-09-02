08a774f832e6bfdfe4ff459851cd843eb8a35901
falsy <me@falsy.me>
2018-09-02 19:19:26 +0900

    v0.0.2
    * selecter
    * eq
    * addClass
    * removeClass
    * (roof)

--- loaf-dom.js
+++ loaf-dom.js
@@ -1,1 +1,41 @@
-console.log('hello world');
+class LoafDom {
+	constructor(element) {
+		this.element = '';
+		if(typeof element === 'string') {
+			if(element[0] === '#') {
+				this.element = document.getElementById(element.substring(1));
+			}
+			if(element[0] === '.') {
+				this.element = document.getElementsByClassName(element.substring(1));
+			}
+		}
+	}
+
+	roof(array, fnc) {
+		const len = array.length;
+		let i;
+		for(i=0; i<len; i++) {
+			fnc(i);
+		}
+	}
+
+	eq(index) {
+		this.element = this.element[index];
+		return this;
+	}
+
+	addClass(className) {
+		this.element.className += ' ' + className;
+		return this;
+	}
+
+	removeClass(className) {
+		const regClassName = new RegExp(className, 'g');
+		this.roof(this.element, () => {
+			this.element[0].className = this.element[0].className.replace(regClassName, '');
+		});
+		return this;
+	}
+}
+
+export default LoafDom;
--- readme.md
+++ readme.md
@@ -3,5 +3,12 @@ DOM Script Library
 
 ## ChangeLog
 
+#### v0.0.2
+* selecter
+* eq
+* addClass
+* removeClass
+* (roof)
+
 #### v0.0.1
 * init
