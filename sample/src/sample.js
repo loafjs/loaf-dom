64410e61cc0fbfacdf417b1bfdf083406dcce8d1
falsy <me@falsy.me>
2018-09-09 14:43:30 +0900

    v0.0.6
    * removeClass selector Error correction

--- sample/src/sample.js
+++ sample/src/sample.js
@@ -4,3 +4,4 @@ L('#wrap').addClass('add-class');
 L('.color-blue').addClass('add-class');
 L('.color').removeClass('color-red');
 L('.color').eq(2).addClass('eq2-add-class');
+L('.color').removeClass('color');
