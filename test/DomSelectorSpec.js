ee81ffb4b8146d70041265ca7255e836d49c0a2c
falsy <me@falsy.me>
2018-10-27 23:56:08 +0900

    modify test spec (selector)

--- test/DomSelectorSpec.js
+++ test/DomSelectorSpec.js
@@ -4,9 +4,9 @@ import Mock from './sampleHTML';
 const newElement = document.createElement("div");
 			newElement.innerHTML = Mock;
 
-document.getElementsByTagName('body')[0].appendChild(newElement);
-
 describe('Test DOM elements with selectors', () => {
+
+	document.getElementsByTagName('body')[0].appendChild(newElement);
 
   it('ID Selector', () => {
   	const loafSelector = L('#id-selector').el();
@@ -32,6 +32,48 @@ describe('Test DOM elements with selectors', () => {
   it('CSS Selector', () => {
   	const loafSelector = L('section > .container figure').el();
   	const domSelector = document.querySelector('section > .container figure');
+
+  	expect(loafSelector).toEqual(domSelector);
+  });
+
+  it('eq Selector', () => {
+  	const loafSelector = L('span').eq(2).el();
+  	const domSelector = document.querySelectorAll('span')[2];
+
+  	expect(loafSelector).toEqual(domSelector);
+  });
+
+  it('next Selector', () => {
+  	const loafSelector = L('main').next().el();
+  	const domSelector = document.getElementsByTagName('main')[0].nextElementSibling;
+
+  	expect(loafSelector).toEqual(domSelector);
+  });
+
+  it('prev Selector', () => {
+  	const loafSelector = L('main').prev().el();
+  	const domSelector = document.getElementsByTagName('main')[0].previousElementSibling;
+
+  	expect(loafSelector).toEqual(domSelector);
+  });
+
+  it('parent Selector', () => {
+  	const loafSelector = L('main').parent().el();
+  	const domSelector = document.getElementsByTagName('main')[0].parentElement;
+
+  	expect(loafSelector).toEqual(domSelector);
+  });
+
+  it('parents Selector', () => {
+  	const loafSelector = L('a').parents('body').el();
+  	const domSelector = document.getElementsByTagName('body')[0];
+
+  	expect(loafSelector).toEqual(domSelector);
+  });
+
+  it('children Selector', () => {
+  	const loafSelector = L('header').children().el();
+  	const domSelector = document.getElementsByTagName('header')[0].children[0];
 
   	expect(loafSelector).toEqual(domSelector);
   });
