6e4eba3acc7fd94a15a09684fa134404ceb578d2
falsy <me@falsy.me>
2018-10-28 00:12:47 +0900

    modify test spec (attribute)

---
+++ test/domAttributeSpec.js
@@ -0,0 +1,41 @@
+import L from '../loaf-dom';
+import Mock from './sampleHTML';
+
+const newElement = document.createElement("div");
+			newElement.innerHTML = Mock;
+
+describe('Test DOM elements with selectors', () => {
+
+	document.getElementsByTagName('body')[0].appendChild(newElement);
+
+  it('addClass(), removeClass()', () => {
+    const className = 'add-class';
+
+  	L('#wrap').addClass(className);
+  	const domAddClassName = document.getElementById('wrap').className;
+
+  	expect(domAddClassName).toEqual(className);
+
+    L('#wrap').removeClass(className);
+    const domRemoveClassName = document.getElementById('wrap').className;
+
+    expect(domRemoveClassName).toEqual('');
+  });
+
+  it('get, set attr()', () => {
+    const key = 'key';
+    const value = 'value';
+    L('#wrap').attr(key, value);
+
+    expect(L('#wrap').attr(key)).toEqual(value);
+  });
+
+  it('get, set style()', () => {
+    const key = 'background';
+    const value = 'blue';
+    L('#wrap').style(key, value);
+
+    expect(L('#wrap').style(key)).toEqual(value);
+  });
+
+});
